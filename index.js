3875a393ef0d63cc988bf1c2d42c36b1d8219cce
Matthew <matthewjaredlevin88@gmail.com>
2023-06-16 14:21:40 -0500

    complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Matt'
+
+function fixTheScope() {
+    return customerName;
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName;
+    
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Mike';
+    return leastFavoriteCustomer;
+
+}
